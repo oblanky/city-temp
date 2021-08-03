9e9bbf6b16bb6215987404f8c7b0ea729da3aeeb
oblanky <87757828+oblanky@users.noreply.github.com>
2021-08-03 14:17:34 +0100

    Add files via upload

---
+++ city-temp-convert.js
@@ -0,0 +1,21 @@
+let city = 'Mumbai';
+let celsius = 23.5;
+let fahrenheit = (celsius*9/5) + 32; // write your expression here
+console.log('Temperature in ' + city + ' is ' + celsius + ' C / ' + fahrenheit + ' F ');
+
+//Part 2
+let city1 = 'Brisbane';
+let celsius1 = 17.0;
+let fahrenheit1 = (celsius1*9/5) +32;
+
+let city2 = 'Delhi';
+let celsius2 = 30.5;
+let fahrenheit2 = (celsius2*9/5) +32;
+
+let city3 = 'London';
+let celsius3 = 12.5;
+let fahrenheit3 = (celsius3*9/5) + 32;
+
+console.log('Temperature in ' + city1 + ' is ' + celsius1 + ' C / ' + fahrenheit1 + ' F ');
+console.log('Temperature in ' + city2 + ' is ' + celsius2 + ' C / ' + fahrenheit2 + ' F ');
+console.log('Temperature in ' + city3 + ' is ' + celsius3 + ' C / ' + fahrenheit3 + ' F ');
